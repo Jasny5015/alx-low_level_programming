f38bcc64173019002a57802a27cf388c601bd1c9
Jasny5015 <wibiyemi@gmail.com>
2022-03-12 04:17:32 -0800

    init

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Prints alphabet from z to a
+ *
+ * Return: Alway return 0 once successful
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n')
+		return (0);
+}
