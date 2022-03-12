014995df52160fc41ad84a4fc6f31bb2d8a8a920
Jasny5015 <wibiyemi@gmail.com>
2022-03-11 17:51:57 -0800

    init

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Prints from 0 to 9
+ *
+ * Return: Always return 0 once successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf('%d', i);
+	}
+	printf("\n");
+	return (0);
+}
