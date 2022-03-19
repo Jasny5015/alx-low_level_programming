de3fd2f7bd113c55854ae51313d3d4b62af63a24
Jasny5015 <wibiyemi@gmail.com>
2022-03-18 20:26:51 -0700

    loop

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper- A function that checks the case of a letter
+ *
+ * Return: returns 1 if it's an uppercase letter, returns 0 otherwise
+ *
+ * @c: an alphabet
+ */
+
+int _isupper(int c)
+
+{
+		return (c >= 'A' && c <= 'Z');
+}
