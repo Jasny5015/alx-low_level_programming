61ad971f09983387abe074a5ae5ea078125cc186
Jasny5015 <wibiyemi@gmail.com>
2022-03-22 21:15:42 -0700

    hmmm

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,9 +3,9 @@
 
 /**
  *print_array - prints n elements of an array of integers
- *@a: A pointer to an int that will be updated/ changed
- *@n: return value n
- *Return: void which ,means our answer is correct
+ *@a: input array.
+ *@n: input n elements
+ *Return: no return
  */
 
 void print_array(int *a, int n)
