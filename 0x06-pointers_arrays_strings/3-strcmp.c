21a01af2202ba074a00313051deab186554e05d4
Jasny5015 <wibiyemi@gmail.com>
2022-03-23 20:57:36 -0700

    pointers array

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strcmp - compares two strings
  * @s1: first string
@@ -16,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if ((*(s1 + 1) == '\0') && (*(s2 + 1 == '\0'))
 			break;
-		op = *(s1 + 1) - *(s2 + 1);
+		op = *(s1 + i) - *(s2 + i);
 		i++;
 	}
 	return (op);
