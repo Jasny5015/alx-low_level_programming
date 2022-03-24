38a981e93b87190d78018f8bd57b8607f54859e9
Jasny5015 <wibiyemi@gmail.com>
2022-03-23 21:39:31 -0700

    pointers array

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -21,10 +21,10 @@ char *cap_string(char *s)
 			{
 			if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
 				*(s + (count + 1) = *(s + (count + 1)) - 32;
-	break;
+						break;
+			}
+		}
+		count++;
 	}
-	}
-count++;
-}
-return (s);
+	return (s);
 }
